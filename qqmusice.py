f51611aa7c984aa8c2e9e9c250d6637abf98f2c9
zhangxiaolei0123 <zhangxiaolei0123@users.noreply.github.com>
2018-09-02 14:07:27 +0800

    Add files via upload

---
+++ qqmusice.py
@@ -0,0 +1,84 @@
+#_*_coding:utf-8_*_
+"""
+  需求：爬取所有qq音乐
+  入口：获取分类歌单 https://y.qq.com/portal/playlist.html
+         获取分类歌单列表 https://y.qq.com/n/yqq/playsquare/2054035442.html#stat=y_new.playlist.pic_click
+         获取每首歌曲详情
+         http://dl.stream.qqmusic.qq.com/C4000037HlEP0subhS.m4a?vkey=9ADBCF5B41C3C81F2B626C7C6D5E85CE5459783FBF33EE885D247B0DBB9CF3DCAE86E53C9A7EB89B91B5E4E35857F87CDE391E5993C87A5D&guid=2438110866&uin=0&fromtag=66
+  代码分析：xpath
+"""
+import  requests
+from  bs4 import BeautifulSoup
+from lxml import etree
+import json
+
+# 获取分类歌单
+# 获取分类歌单列表
+# 获取每首歌曲详情
+# 保存获取每首歌曲详情
+def get_classification_list(url,headers):
+    r=requests.get(url,headers=headers).content.decode('utf-8')
+    print(r)
+    html=etree.HTML(r)
+    mulislist1=html.xpath("//span[@class='playlist__title_txt']/a")
+    print(mulislist)
+    mulislist=html.xpath("//span[@class='playlist__title_txt']/a[@class='js_playlist']/@href")
+    print(mulislist1)
+    return mulislist
+if __name__=="__main__":
+    heads = {
+        'referer':'https://y.qq.com/portal/playlist.html',
+        'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3371.0 Safari/537.36'
+    }
+    url='https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg?picmid=1&rnd=0.34692207151847465&g_tk=5381&jsonpCallback=getPlaylist&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&categoryId=10000000&sortId=5&sin={0}&ein={1}'
+    sin=0
+    ein=29
+    page=1 #页码数
+    while True:
+        html=requests.get(url.format(sin,ein),headers=heads).text
+        dissidList=json.loads(html.strip('getPlaylist()'))#第一页获取歌曲列表
+        classmusie=1
+        for list in dissidList['data']['list']:
+            dissid=list['dissid'] #获取歌单id
+            dissname=list['dissname']#获取歌单名称
+            songmidurl='https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg?type=1&json=1&utf8=1&onlysong=0&disstid={0}&format=jsonp&g_tk=5381&jsonpCallback=playlistinfoCallback&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0'
+            heads['referer']='https://y.qq.com/n/yqq/playsquare/2054035442.html'
+            html = requests.get(songmidurl.format(dissid), headers=heads).text
+            song_dic=json.loads(html.strip('playlistinfoCallback()'))
+            k=1# 歌曲
+            for songmids in song_dic['cdlist'][0]['songlist']:
+                songmid=songmids['songmid']#获取songmid
+                songname=songmids['songname']#获取songmid
+                filename='C400{0}.m4a'.format(songmid)
+                vkeyurl='https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg?g_tk=5381&' \
+                        'jsonpCallback = MusicJsonCallback' \
+                        '&loginUin=0&hostUin=0&format=json&inCharset=utf8' \
+                        '&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0' \
+                        '&cid=205361747&callback=MusicJsonCallback' \
+                        '&uin=0&songmid={0}&' \
+                        'filename={1}&guid=1093240106'
+                heads['referer'] = 'https://y.qq.com/portal/playlist.html'
+                vkhtml = requests.get(vkeyurl.format(songmid,filename), headers=heads).text
+                vkdict=json.loads(vkhtml.strip('MusicJsonCallback()'))
+                vkey=vkdict['data']['items'][0]['vkey'] #提取vkey
+                #通过vkey下载音乐
+                musicurl='http://dl.stream.qqmusic.qq.com/C400{0}.m4a?vkey={1}&guid=1093240106&uin=0&fromtag=66'
+                heads['Host']='dl.stream.qqmusic.qq.com'
+                del heads['referer']
+                musicurhtml=requests.get(musicurl.format(songmid,vkey),headers=heads,stream=True)
+                try:
+                    with open("music/music{0}.mp3".format(songname),'wb') as file:
+                        for i in musicurhtml.iter_content(1024*10): #每次读取1024
+                            file.write(i) #每次写1024
+                except OSError:
+                    continue
+                print("第{0}页，分类{1},第{2}首歌{3}".format(page,classmusie,k,songname))
+                k+=1
+            classmusie+=1
+        #下一页
+        sin+=30
+        ein+=30
+        page+=1
+        if sin<6075:
+            break
+
---
+++ zhiLian.py
@@ -0,0 +1,117 @@
+#_*_coding:utf-8_*_
+"""
+需求分析：
+    获取职位名称等。。。。
+入口：
+    http://sou.zhaopin.com/
+    //div[@id='search_right_demo']/div/div/a/@href   获取职位列表url
+    //td[@class='zwmc']/div/a[1]      获取详细职位列表url
+    //a[@class='next-page']           下一页
+    //div[@class='inner-left fl']/h1   获取标题
+    //div[@class='inner-left fl']/h2   公司名称
+    //div[@class='welfare-tab-box']     公司福利   返回文本，text()返回列表
+    //ul[@class='terminal-ul clearfix']/li  获取职位信息 8 个
+
+    代码
+"""
+# import requests
+# from lxml import etree
+# headers={
+# 'Referer': 'http://sou.zhaopin.com/',
+# 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3371.0 Safari/537.36'
+# }
+# requestsURL='http://sou.zhaopin.com/'
+# zhlian=requests.get(requestsURL,headers=headers)
+# html=etree.HTML(zhlian.text)
+# print(html)
+# urlList=html.xpath("//div[@id='search_right_demo']/div/div/a/@href")
+# print(urlList)
+# for url in urlList:
+#     zhlian2 = requests.get(requestsURL+url, headers=headers)
+#     html2=etree.HTML(zhlian2.text)
+#     urlList2=html2.xpath("//td[@class='zwmc']/div/a[1]/@href")
+#     print(urlList2)
+#     for url2 in urlList2:
+#         zhlian3=requests.get(requestsURL + url, headers=headers)
+#         html3 = etree.HTML(zhlian3.text)
+#         title=html2.xpath("//div[@class='inner-left fl']/h1")
+#         gsmc=html2.xpath("//div[@class='inner-left fl']/h2")
+#         print("*"*50)
+#         print(title)
+# 1,获取所有职位分类列表
+import requests
+from lxml import etree
+import re
+def get_job_cat_list(url,headers):
+    r=requests.get(url, headers=headers).content.decode('utf-8')
+    html=etree.HTML(r)
+    job_cat_list=html.xpath("//div[@id='search_right_demo']/div/div/a/@href")
+    pattern=re.compile('jl=\d+&')
+    job_cat_list=[url[:-1]+str(pattern.sub('jl=489',i)) for i in job_cat_list]
+    return  job_cat_list
+# 2,获取所有职位列表(返回页码和列表)
+def get_job_list(url,headers):#url 跳转列表url
+    r = requests.get(url, headers=headers).content.decode('utf-8')
+    html = etree.HTML(r)
+    zwxqlist=html.xpath("//td[@class='zwmc']/div/a[1]/@href")#页面的url
+    next_page=html.xpath("//a[@class='next-page']/@href")
+    if len(next_page):
+        return zwxqlist ,next_page[0]
+    else:
+        return zwxqlist, next_page
+    # zwxqlist = html.xpath("//td[@class='zwmc']/div/a[1]")  # 页面的url
+    # zwxqlist = html.xpath("//a[@class='next-page nopress2']")  # 最后一页的url
+    pass
+# 3,获取职位详细信息
+def get_job_info(url,headers):
+    r = requests.get(url, headers=headers).content.decode('utf-8')
+    html = etree.HTML(r)
+    job_dic={}
+    job_dic['zwmc']=html.xpath("string(//div[@class='inner-left fl'or @class='fl']/h1)")
+    job_dic['gsmc']=html.xpath("string(//div[@class='inner-left fl'or @class='fl']/h1)")
+    job_dic['gsfl']=html.xpath("//div[@class='inner-left fl']/div/span/text()")
+    job_dic['zwyx']=html.xpath("string(//div[@class='terminalpage-left']/ul/li[1]/strong)")
+    job_dic['gzdd']=html.xpath("string(//div[@class='terminalpage-left']/ul/li[3]/strong)")
+    job_dic['fbrq']=html.xpath("string(//div[@class='terminalpage-left']/ul/li[3]/strong)")
+    job_dic['gzxz']=html.xpath("string(//div[@class='terminalpage-left']/ul/li[4]/strong)")
+    job_dic['gzjy']=html.xpath("string(//div[@class='terminalpage-left']/ul/li[5]/strong)")
+    job_dic['zdxl']=html.xpath("string(//div[@class='terminalpage-left']/ul/li[6]/strong)")
+    job_dic['zprs']=html.xpath("string(//div[@class='terminalpage-left']/ul/li[7]/strong)")
+    job_dic['zwlb']=html.xpath("string(//div[@class='terminalpage-left']/ul/li[8]/strong)")
+    return job_dic
+# 4，保存数据
+def save_data(data):
+    data=','.join([str(i) for i in data.values()])
+    with open('zl.txt','a+',encoding='utf-8')as file:
+        file.write(data)
+    pass
+if __name__=='__main__':
+    url="http://sou.zhaopin.com/"
+    headers={
+    'Referer': 'http://sou.zhaopin.com/',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3371.0 Safari/537.36'
+    }
+    job_cat_list=get_job_cat_list(url,headers)
+
+    for j in job_cat_list:
+        page = 1
+        job_sum_list, next_page=get_job_list(j,headers)
+        print('第{0}页，第60条'.format(page))
+        print(len(job_sum_list))
+        while next_page:
+            page += 1
+            job_list, next_page = get_job_list(next_page, headers)
+            print('第{0}页，第60条'.format(page))
+            print(len(job_list))
+            job_sum_list += job_list
+        else:# 只获取第一个url中所有数据列表
+            break
+    print(len(job_sum_list)) #60 * 页码数量
+    zhiliandatals=[]
+    for i in job_list:
+        print(i)
+        job_dic=get_job_info(i,headers)
+        with open('zl.txt', 'a+', encoding='utf-8')as file:
+            file.write(str(job_dic))
+        zhiliandatals.append(job_dic)
+        print(job_dic)
