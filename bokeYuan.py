5b3371cda822ab522c1be79c9a5b8bc9c62b0628
zhangxiaolei0123 <zhangxiaolei0123@users.noreply.github.com>
2018-09-04 13:13:42 +0800

    Update bokeYuan.py

--- bokeYuan.py
+++ bokeYuan.py
@@ -3,21 +3,21 @@
 需求分析
 获取博客园每一篇帖子标题和内容
 源码分析
-入口："https://www.cnb.com/"
+入口："https://www.cn.com/"
   1、获取标题的url   //a[@class='titlelnk']
   2、通过URL访问每一篇帖子的详细内容，获取标题和内容
 
   //a[@id='cb_post_title_url']/text()
   String(div[@id='cnblogs_post_body'])
   第二页的url
-  https://www.cnblogs.com/#p2
+  https://www.cn.com/#p2
 """
 import numpy as np
 import pandas as pd
 import requests
 from  bs4 import BeautifulSoup
 from lxml import etree
-requestUrl="https://www.cns.com/"
+requestUrl="https://www.cn.com/"
 headers={
 'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3371.0 Safari/537.36'
 }
@@ -26,7 +26,7 @@ num=1  #帖子的数量
 while True:
        html=requests.get(requestUrl,headers=headers)
        html=etree.HTML(html.text)
-       # https: // www.cnb.com / sitehome / p / 190
+       # https: // www.cn.com / sitehome / p / 190
        urllist=html.xpath("//a[@class='titlelnk']/@href")#文章列表
        for url in urllist:
             print("第{0}页,第{1}条".format(page,num))
